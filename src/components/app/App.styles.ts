09c6cc062ecc6beeecf02cd1c828690b397969b7
Mateusz Wojtania <mateusz99w@gmail.com>
2022-06-26 18:31:56 +0200

    adding styled-components functionality

---
+++ src/components/app/App.styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+fint-size: 1em;
+color: red;
+
+`;
--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,34 +1,34 @@
-import React from 'react';
-import logo from './res/logo.svg';
-import { Counter } from '../counter/Counter';
 import { Heroes } from '../heroes/heroes';
 import { Hero } from '../heroe/hero';
-import './App.css';
+import { Title } from './App.styles';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App" style={{ borderStyle: 'solid', display: 'flex', flexDirection: 'row' }}>
+    <React.Fragment>
+      <Title>Adventure Game!</Title>
+      <div className="App" style={{ borderStyle: 'solid', display: 'flex', flexDirection: 'row' }}>
+        <div id='side-panel' style={{ borderStyle: 'solid', display: 'flex', flexDirection: 'column' }}>
+          <Heroes /> {/*przerob na liste komponentow hero */}
 
-      <div id='side-panel' style={{ borderStyle: 'solid', display: 'flex', flexDirection: 'column' }}>
-        <Heroes /> {/*przerob na liste komponentow hero */}
+          <button id='generate-mob'>Zmiana stworka</button>                                 {/*React component*/}
+          <button id='region'>Zmiana regionu</button>                                       {/*React component*/}
+        </div>
 
-        <button id='generate-mob'>Zmiana stworka</button>                                 {/*React component*/}
-        <button id='region'>Zmiana regionu</button>                                       {/*React component*/}
-      </div>
+        <div id='main'>
+          <div id='hero-creator' style={{ borderStyle: 'solid' }}>Tworzenie postaci</div>   {/*React component*/}
+          <div id='wrapper' style={{ display: 'flex', flexDirection: 'row' }}>
+            <div id='game'>
+              <Hero />
 
-      <div id='main'>
-        <div id='hero-creator' style={{ borderStyle: 'solid' }}>Tworzenie postaci</div>   {/*React component*/}
-        <div id='wrapper' style={{ display: 'flex', flexDirection: 'row' }}>
-          <div id='game'>
-            <Hero />
+              <div id='run' style={{ borderStyle: 'solid' }}>Tu jest atak!</div>            {/*React component*/}
+            </div>
 
-            <div id='run' style={{ borderStyle: 'solid' }}>Tu jest atak!</div>            {/*React component*/}
+            <div id='arsenal' style={{ borderStyle: 'solid' }}>Tu jest arsenal</div>        {/*React component*/}
           </div>
-
-          <div id='arsenal' style={{ borderStyle: 'solid' }}>Tu jest arsenal</div>        {/*React component*/}
         </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 }
 
